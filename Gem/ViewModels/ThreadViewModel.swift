a089b11fb09e28d4c4bc83328bea9f4f268d661c
Jojo F <georgefung78@live.com>
2026-04-28 00:20:18 -0700

    fix: haptics on thread screen (#131)

--- Gem/ViewModels/ThreadViewModel.swift
+++ Gem/ViewModels/ThreadViewModel.swift
@@ -132,6 +132,7 @@ import Translation
             HapticsManager.shared.stop()
             saveToCache()
             self.status = .completed
+            self.translationStatus = .completed
             self.comments = commentsBuffer
         }
         
@@ -196,7 +197,9 @@ import Translation
             commentsBuffer[index] = comment
             
             if isTranslationEnabled {
+                HapticsManager.shared.stop()
                 status = .backgroundLoading
+                translationStatus = .inProgress
                 comments.append(comment)
             }
         }
