e0490211348936685313e684f826d72b2c87098b
Jojo Feng <georgefung78@live.com>
2026-05-09 02:50:53 -0700

    update

--- Gem/Views/Thread/Components/TimeMachineTutorialSheet.swift
+++ Gem/Views/Thread/Components/TimeMachineTutorialSheet.swift
@@ -3,31 +3,6 @@ import SwiftUI
 
 extension Thread {
     struct TimeMachineTutorialSheet: View {
-        @State private var model = PlayerModel()
-        
-        var body: some View {
-            ZStack(alignment: .top) {
-                AVPlayerView(player: model.player)
-                    .ignoresSafeArea(edges: .bottom)
-                    .padding(.top, 48)
-                    .onAppear { model.player.play() }
-                    .onDisappear { model.player.pause() }
-                
-                VStack(alignment: .center) {
-                    Text("Time Machine")
-                        .font(.title)
-                        .foregroundStyle(.white)
-                        .padding(.top, 24)
-                    Text("Swipe from right edge to left on a comment to see its ancestors")
-                        .font(.body)
-                        .foregroundStyle(.white)
-                }
-            }
-        }
-    }
-    
-    @Observable
-    class PlayerModel {
         let player: AVQueuePlayer
         let looper: AVPlayerLooper
         
@@ -40,6 +15,26 @@ extension Thread {
                 looper = AVPlayerLooper(player: queuePlayer, templateItem: AVPlayerItem(url: URL(fileURLWithPath: "")))
             }
             player = queuePlayer
+        }
+        
+        var body: some View {
+            ZStack(alignment: .top) {
+                AVPlayerView(player: player)
+                    .ignoresSafeArea(edges: .bottom)
+                    .padding(.top, 48)
+                    .onAppear { player.play() }
+                    .onDisappear { player.pause() }
+                
+                VStack(alignment: .center) {
+                    Text("Time Machine")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .padding(.top, 24)
+                    Text("Swipe from right edge to left on a comment to see its ancestors")
+                        .font(.body)
+                        .foregroundStyle(.white)
+                }
+            }
         }
     }
     
